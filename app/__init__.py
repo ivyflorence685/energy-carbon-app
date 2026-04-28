42f00c73fa58512186658e7ca009f9e3fdc04a17
Ivy florence <ivyflorence212006@gmail.com>
2026-04-28 22:32:24 +0530

    Updated __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    app.config["DATA_PATH"] = os.path.join(BASE_DIR, "..", "public", "data", "appliance_usage.json")
+
+    from .routes import main_routes
+    app.register_blueprint(main_routes)
+
+    return app
