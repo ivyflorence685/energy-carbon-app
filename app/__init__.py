38f78b5090424049da0b6868cbb2699ce2bdd258
Ivy florence <ivyflorence212006@gmail.com>
2026-05-18 17:50:03 +0530

    reorganised files and created new folders

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     app.config["DATA_PATH"] = os.path.join(BASE_DIR, "..", "public", "data", "appliance_usage.json")
 
-    from .routes import main_routes
+    from app.routes.main_routes import main_routes
     app.register_blueprint(main_routes)
 
     return app
